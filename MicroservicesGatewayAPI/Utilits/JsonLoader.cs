02a64116921ab57d94b2fd302885c2c5b191867d
Nikivas <nikivas97@mail.ru>
2018-08-15 09:55:48 +0400

    json with routes and jsonLoader

---
+++ MicroservicesGatewayAPI/Utilits/JsonLoader.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroservicesGatewayAPI.Utilits
+{
+    public class JsonLoader
+    {
+        public static T LoadFromFile<T>(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                T result = JsonConvert.DeserializeObject<T>(json);
+                return result;
+            }
+                
+        }
+
+        public T Deserialize<T>(object json)
+        {
+            return JsonConvert.DeserializeObject<T>(Convert.ToString(json));
+        }
+
+    }
+}
